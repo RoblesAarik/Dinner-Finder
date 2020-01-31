5b8dd52a77a9395151fbd4dadea1f9c40ea924cb
Aarik Robles <roblesaarik@gmail.com>
2020-01-31 13:56:35 -0800

    updated HTML and CSS

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 console.log("Works");
 $(() => {
-  $.ajax({
-    url: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772",
-  }).then(
-    data => {
-      console.log(data);
-      $("#recipe1").append(data.idMeal);
-    },
-    () => {
-      console.log("bad");
-    }
-  );
+  $("form").on("submit", () => {
+    event.preventDefault();
+
+    const userInput = $('input[type="text"]').val();
+
+    $.ajax({
+      url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian",
+    }).then(
+      data => {
+        console.log(data);
+        // $("#recipe1").append(data.strMeal);
+      },
+      () => {
+        console.log("bad");
+      }
+    );
+  });
 });
